2400d7151391a7b88514fe1aa64cc6dc9e9096b3
Vani Thilagar <vthilagar@parallelwireless.com>
2020-05-14 23:30:25 -0400

    Final checkin

--- p3/src/components/AddItemPage.vue
+++ p3/src/components/AddItemPage.vue
@@ -2,9 +2,9 @@
   <div>
     <h2>Add a new Item</h2>
 
-    <label for='title'>Title</label>
+    <label for='title'>Title <small>6 chars min</small></label>
     <input type='text' v-model='$v.item.title.$model' id='title'
-    :class='{"form-input-error": $v.item.title.$error}' /><small>6 chars min</small><br />
+    :class='{"form-input-error": $v.item.title.$error}' /><br />
     <div v-if='$v.item.title.$error'>
       <div class='form-feedback-error'>
         <div v-if='!$v.item.title.required'>
@@ -17,9 +17,19 @@
     </div>
 
     <label for='type'>Item type</label>
-    <input type='text' v-model='item.type' id='type' />
+    <input type='text' v-model='$v.item.type.$model' id='type' :class='{"form-input-error": $v.item.type.$error}'  />
+    <div v-if='$v.item.type.$error'>
+      <div class='form-feedback-error'>
+        <div v-if='!$v.item.type.required'>
+          Title is required
+        </div>
+        <div v-else>
+          Type has to be alphabetic chars
+        </div>
+      </div>
+    </div>
 
-    <label for='duration'>Performance time(in minutes):</label>
+    <label for='duration'>Performance time(in minutes)  <small>2-6 minutes</small></label>
     <input type='text' v-model='$v.item.duration.$model' id='duration'
     :class='{"form-input-error": $v.item.duration.$error}'  />
     <div v-if='$v.item.duration.$error'>
@@ -36,10 +46,21 @@
       </div>
     </div>
     <label for='performers'>Performers(Enter comma separated names)</label>
-    <input type='text' v-model='item.performers' id='performers'  />
-
+    <input type='text' v-model='$v.item.performers.$model' id='performers'
+    :class='{"form-input-error": $v.item.performers.$error}'  />
+    <div v-if='$v.item.performers.$error'>
+      <div class='form-feedback-error'>
+        Performer names are required
+      </div>
+    </div>
     <label for='description'>Description</label>
-    <textarea v-model='item.description' id='description'></textarea>
+    <textarea v-model='$v.item.description.$model' id='description'
+    :class='{"form-input-error": $v.item.description.$error}' ></textarea>
+    <div v-if='$v.item.description.$error'>
+      <div class='form-feedback-error'>
+        Item description is required
+      </div>
+    </div>
     <div class='form-feedback-error' v-if='$v.$anyError'>Please correct the form errors</div>
     <input type='submit' @click='addNewItem()' value='Add' /><br />
     <transition name='fade'>
@@ -47,14 +68,14 @@
       data-test='item-added-confirmation'
       class='alert'
       v-if='added'
-      >New item {{ item.title }} was added!</div>
+      >New item  was added!</div>
     </transition>
   </div>
 </template>
 
 <script>
 import * as app from '@/common/app.js';
-import { required, minLength, integer, minValue, maxValue } from 'vuelidate/lib/validators'
+import { required, minLength, integer, minValue, maxValue, alpha } from 'vuelidate/lib/validators'
 //import * as utils from '@/common/utils';
 export default {
   name: '',
@@ -80,6 +101,16 @@ export default {
         integer,
         minValue: minValue(2),
         maxValue: maxValue(6)
+      },
+      type: {
+        required,
+        alpha
+      },
+      performers: {
+        required
+      },
+      description: {
+        required
       }
 
     }
@@ -98,7 +129,7 @@ export default {
 
               this.added = true;
 
-              setTimeout(() => (this.added = false), 3000);
+              setTimeout(() => (this.added = false), 4000);
 
               this.item = {
                 title: '',
